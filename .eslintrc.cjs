f49a97da7d1b29ac0383a95d18177179c331113a
peakercope <peakercope@gmail.com>
2023-07-04 11:29:17 +0400

    test lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,32 @@
 module.exports = {
+  root: true,
   extends: [
-    "plugin:@typescript-eslint/recommended",
     "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended",
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:svelte/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/strict"
   ],
+  parserOptions: {
+    project: "./tsconfig.json",
+    extraFileExtensions: [".astro", ".svelte"],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true
+    }
+  },
+  rules: {
+    "import/prefer-default-export": "off",
+  },
   overrides: [
     {
       files: ["*.astro"],
       parser: "astro-eslint-parser",
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: "@typescript-eslint/parser"
       },
     },
     {
@@ -17,6 +34,16 @@ module.exports = {
       parserOptions: {
         sourceType: 'module',
       },
+    },
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+      rules: {
+        "import/no-mutable-exports": "off"
+      }
     },
   ],
 }
--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -1,9 +1,12 @@
-<script>
-  export let title;
-  export let content;
-  export let _embedded;
+<script lang="ts">
+  import type { Post } from '@wordpress/core-data';
+
+  export let title: Post['title'];
+  export let content: Post['content'];
+  // export let _embedded;
 </script>
 
+<!--
 {#if _embedded?.['wp:featuredmedia']?.['0']?.source_url}
   <img
     alt={title.rendered}
@@ -11,11 +14,11 @@
     src={_embedded['wp:featuredmedia']['0'].source_url}
   />
 {/if}
-
-
-{#if content?.rendered}
+ -->
+<span>{title}</span>
+{#if content.rendered}
   <div>
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
     {@html content.rendered}
   </div>
 {/if}
-
