4cc16b9f3aa13481eda32729027c5f5159e2d234
peakercope <peakercope@gmail.com>
2023-07-04 18:02:11 +0400

    feat: add svelte lint and check (#4)

--- src/components/Appearance.svelte
+++ src/components/Appearance.svelte
@@ -1,3 +1,6 @@
+<script lang="ts">
+</script>
+
 <div>
   <slot />
 </div>
--- src/service/data.ts
+++ src/service/data.ts
@@ -1,20 +1,19 @@
-import type {Post} from '@wordpress/core-data';
+import type { Post } from '@wordpress/core-data';
 
 export const getPostsSlug = async ():Promise<Post[]> => {
   const data = await fetch(`${import.meta.env.PUBLIC_WP_API}/posts?_fields=slug,id,title`);
 
-  return await data.json();
-}
+  return data.json();
+};
 
 export const getPosts = async ():Promise<Post[]> => {
   const res = await fetch(`${import.meta.env.PUBLIC_WP_API}/posts`);
 
-  return await res.json();
-}
+  return res.json();
+};
 
 export const getPost = async (id: number):Promise<Post> => {
   const res = await fetch(`${import.meta.env.PUBLIC_WP_API}/posts/${id}?_embed`);
 
-  return await res.json();
-}
-
+  return res.json();
+};
