16077726faa16227d96dbf299b407e8e8b1b790b
Mandrik Vladimir <mandrikvova@gmail.com>
2023-11-21 20:47:59 +0300

    feat: refactored code

--- src/entities/page/ui/Page/Page.svelte
+++ src/entities/page/ui/Page/Page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-  import { PageType } from "@/entities/page";
+  import type { PageType } from '@/entities/page';
 
   export let content: PageType['content'];
-  export let elements = [];
-
-  console.log(elements);
+  export let elements: {
+    id: number
+    name: string
+    attributes: any
+    parent: any
+  }[];
 </script>
 
 {#if content.rendered}
@@ -13,3 +16,12 @@
     {@html content.rendered}
   </div>
 {/if}
+
+{#each elements as element}
+    <div>
+        {element.name}
+        {@html element.parent}
+        <!--{element.attributes}-->
+        <!--{element.parent}-->
+    </div>
+{/each}
